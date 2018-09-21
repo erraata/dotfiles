12a3b2a7d7c292712a6ad5be9637c2130f4c476a
errata <stigmataerrata@gmail.com>
2018-09-21 13:39:38 -0500

    1

--- .config/bspwm/sidebar-youtube.sh
+++ .config/bspwm/sidebar-youtube.sh
@@ -9,5 +9,5 @@ wmctrl -i -r $(xdotool search --onlyvisible --name Firefox) -e 0,2810,1450,1020,
 bspc rule -a Firefox -o state=floating sticky=on
 firefox -new-window "https://keep.google.com" &
 
-sleep 2
+sleep 4
 wmctrl -i -r $(xdotool search --onlyvisible --name Keep) -e 0,2810,535,1020,905
